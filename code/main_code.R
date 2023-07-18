34b20a5a7ea99e9d29216adf65ed3eda7bdab72a
Caitlin Allen Akselrud NOAA <caitlin.allen_akselrud@noaa.gov>
2023-07-18 16:15:45 -0700

    setting up sim code using WHAM

--- code/main_code.R
+++ code/main_code.R
@@ -3,3 +3,16 @@
 # contact: caitlin.allen_akserud@noaa.gov
 # date created: 13.07.2023
 # version: 1.0
+
+devtools::install_github("timjmiller/wham", dependencies=TRUE)
+library(tidyverse)
+library(wham)
+library(here)
+
+# base = Ex 1 model 3 (m3)
+
+# add environmental covariates
+
+# try changing sigmaR and sigma_olderages for survival effects
+
+# remember: you want combos of high/low AC and high/low variance
