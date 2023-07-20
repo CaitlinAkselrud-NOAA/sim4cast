508a6b49df5fb8743e6d7c5db762e779bb7fcaa2
Caitlin Allen Akselrud NOAA <caitlin.allen_akselrud@noaa.gov>
2023-07-20 15:02:44 -0700

    build: setting up simulation code structure

--- code/main_code.R
+++ code/main_code.R
@@ -4,15 +4,31 @@
 # date created: 13.07.2023
 # version: 1.0
 
-devtools::install_github("timjmiller/wham", dependencies=TRUE)
+# devtools::install_github("timjmiller/wham", dependencies=TRUE)
+
+
+# libraries ---------------------------------------------------------------
 library(tidyverse)
 library(wham)
 library(here)
 
+# data --------------------------------------------------------------------
+
+
+# environmental covariates ------------------------------------------------
+
+
+# fit wham model ----------------------------------------------------------
 # base = Ex 1 model 3 (m3)
-
-# add environmental covariates
-
+# use example 2 to set up models with diff cov effects on recr
 # try changing sigmaR and sigma_olderages for survival effects
 
+# simulate wham time series -----------------------------------------------
+
+
+# check variance and autocorrelation --------------------------------------
 # remember: you want combos of high/low AC and high/low variance
+
+# save time series output -------------------------------------------------
+
+
